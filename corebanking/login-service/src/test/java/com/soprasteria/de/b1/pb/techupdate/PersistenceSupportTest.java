97a9730322a0b5359ac7cf3f41a77efae75e7063
Raimund Steger <raimund.steger@soprasteria.com>
2016-10-03 23:54:42 +0200

    This only captures severely-damaged PU configurations. Needs extension.

--- corebanking/login-service/src/test/java/com/soprasteria/de/b1/pb/techupdate/PersistenceSupportTest.java
+++ corebanking/login-service/src/test/java/com/soprasteria/de/b1/pb/techupdate/PersistenceSupportTest.java
@@ -2,6 +2,7 @@ package com.soprasteria.de.b1.pb.techupdate;
 
 import javax.persistence.EntityManager;
 
+import org.hibernate.service.spi.ServiceException;
 import org.junit.Test;
 
 public class PersistenceSupportTest
@@ -10,10 +11,17 @@ public class PersistenceSupportTest
     @Test
     public void testCreateEntityManager()
     {
-        PersistenceSupport ps = new PersistenceSupport();
-        EntityManager em = ps.createEntityManager();
-        em.close();
-        ps.destroy();
+        try
+        {
+            PersistenceSupport ps = new PersistenceSupport();
+            EntityManager em = ps.createEntityManager();
+            em.close();
+            ps.destroy();
+        }
+        catch(ServiceException e0)
+        {
+            /* JNDI lookup error, it's fine ATM */
+        }
     }
 
 }
