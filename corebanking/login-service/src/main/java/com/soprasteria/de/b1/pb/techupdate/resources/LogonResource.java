1da4e020c8098cd9bc6c6fe5bcea8e0640f5a870
Raimund Steger <rs@mytum.de>
2016-10-06 23:12:01 +0200

    Removed compilation warning

--- corebanking/login-service/src/main/java/com/soprasteria/de/b1/pb/techupdate/resources/LogonResource.java
+++ corebanking/login-service/src/main/java/com/soprasteria/de/b1/pb/techupdate/resources/LogonResource.java
@@ -4,7 +4,6 @@ import java.util.logging.Logger;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
