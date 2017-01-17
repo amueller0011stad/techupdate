bb824f2c2836ab043d83b4d44175694aa27371d8
ChristianAdam <christian.adam@soprasteria.com>
2017-01-17 21:04:15 +0100

    finalized schema

--- tecdemo/db_scripts/create_schema.sql
+++ tecdemo/db_scripts/create_schema.sql
@@ -1,7 +1,7 @@
 
 DROP TABLE emplyoee;
 CREATE TABLE employee
-(id int,
+(id INT,
  employeenumber INT,
  login VARCHAR(255),
  firstname VARCHAR(255),
@@ -10,7 +10,7 @@ CREATE TABLE employee
 
 DROP TABLE customer;
 CREATE TABLE customer
-(id int,
+(id INT,
  customerenumber INT,
  login VARCHAR(255),
  customertypeid INT,
@@ -22,9 +22,68 @@ CREATE TABLE customer
 
 DROP TABLE customertype;
 CREATE TABLE customertype
-(id int,
- employeenumber INT,
- login VARCHAR(255),
- firstname VARCHAR(255),
- lastname VARCHAR(255)
+(id INT,
+ description VARCHAR(255)
+);
+
+DROP TABLE login;
+CREATE TABLE login
+(id INT,
+ username VARCHAR(255),
+ passwordhash VARCHAR(255),
+ salt VARCHAR(255),
+ failedloginattempts INT,
+ validuntil DATE,
+ locked BIT
+);
+
+DROP TABLE account;
+CREATE TABLE account
+(id INT,
+ accountnumber INT,
+ accounttypeid INT,
+ customerid INT,
+ description VARCHAR(255)
+);
+
+DROP TABLE accounttype;
+CREATE TABLE accounttype
+(id INT,
+ description VARCHAR(255),
+ currencyid INT,
+ interestrate DOUBLE,
+ annualfee DOUBLE
+);
+
+DROP TABLE currency;
+CREATE TABLE currency
+(id INT,
+ description VARCHAR(255)
+);
+
+DROP TABLE banktransaction;
+CREATE TABLE banktransaction
+(id INT,
+ transactiontypeid INT,
+ senderbankid INT,
+ senderaccountid INT,
+ recipientbankid INT,
+ recipientaccountid INT,
+ amount DOUBLE,
+ transactiontime TIMESTAMP,
+ description VARCHAR(255)
+);
+
+DROP TABLE transactiontype;
+CREATE TABLE transactiontype
+(id INT,
+ description VARCHAR(255)
+);
+
+DROP TABLE bank;
+CREATE TABLE bank
+(id INT,
+ banknumber INT,
+ description VARCHAR(255),
+ serveraddress VARCHAR(1023),
 );
